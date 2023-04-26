72cb3bf5430134c936a9ffdcb9169c303e0991e3
t0nik <aqwrolds202@gmail.com>
2023-04-26 18:45:18 +0200

    Display numbers

--- script.js
+++ script.js
@@ -30,3 +30,20 @@ function operate(first, second, operator) {
       return divide(first, second);
   }
 }
+
+const numbers = document.querySelectorAll(".number");
+const displayField = document.querySelector(".display");
+
+numbers.forEach((num) => {num.addEventListener('click', display)});
+
+// Display character limit
+let numCount = 0;
+const displayLimit = 11;
+
+function display(numberClick) {
+  if (numCount < displayLimit) {
+    displayField.textContent += numberClick.target.textContent;
+    ++numCount;
+  }
+}
+
